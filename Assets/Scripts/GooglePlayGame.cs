0955e3eb41bbb3f1e766dfe7402f9ee1834fef11
Matheus H C Oliveira <matheus.euf@gmail.com>
2021-09-23 13:01:31 -0300

    worst commit

---
+++ Assets/Scripts/GooglePlayGame.cs
@@ -0,0 +1,71 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames.BasicApi;
+using System;
+
+public class GooglePlayGame : MonoBehaviour
+{
+    public static void Init()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+    }
+
+    public static void Login(Action<bool> onLogin)
+    {
+
+        if (Social.Active == null)
+        {
+            return;
+        }
+
+        if (IsAuthenticated())
+        {
+            return;
+        }
+        PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptOnce, (result) =>
+        {
+            onLogin(true);
+        });
+    }
+
+    public static bool IsAuthenticated()
+    {
+        return Social.localUser.authenticated;
+    }
+
+    public static void ReportScore(string scoreID, int score,
+
+        Action<bool> onReportScore)
+
+    {
+
+        // post score
+
+        Social.ReportScore(score, scoreID, (bool success) =>
+        {
+
+            if (onReportScore != null)
+
+            {
+
+                onReportScore(success);
+
+            }
+
+        });
+
+    }
+    public static void ShowLeadboards()
+    {
+        PlayGamesPlatform.Instance.ShowLeaderboardUI("CgkIkP301eQEEAIQAA");
+
+    }
+
+}
---
+++ Assets/Scripts/Minus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minus : MonoBehaviour {
+
+	public TextMesh s;
+
+	void OnMouseDown(){
+		Player.speed -= 0.02f;
+		s.text = Plus.i + Player.speed * 100;
+	}
+}
